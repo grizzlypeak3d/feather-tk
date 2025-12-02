541de4c72a0dfdfaac9d4cb982d5f6b4d0f4b074
Darby Johnston <darbyjohnston@yahoo.com>
2025-12-01 16:28:21 -0800

    Add Python bindings

--- lib/ftk/CorePy/IApp.cpp
+++ lib/ftk/CorePy/IApp.cpp
@@ -16,9 +16,18 @@ namespace ftk
 {
     namespace python
     {
+        class PyIApp : public IApp
+        {
+        public:
+            virtual void run() override
+            {
+                PYBIND11_OVERRIDE(void, IApp, run);
+            }
+        };
+        
         void iApp(py::module_& m)
         {
-            py::class_<IApp, std::shared_ptr<IApp> >(m, "IApp")
+            py::class_<IApp, std::shared_ptr<IApp>, PyIApp>(m, "IApp")
                 .def_property_readonly("exeName", &IApp::getExeName)
                 .def_property_readonly("exitValue", &IApp::getExit)
                 .def_property_readonly("context", &IApp::getContext)
