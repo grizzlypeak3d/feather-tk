e2baad6a9943d26acbe8a0846851a2a434257277
Darby Johnston <darbyjohnston@yahoo.com>
2026-04-02 09:44:37 -0700

    Build fix

--- lib/ftk/UI/AppGL.cpp
+++ lib/ftk/UI/AppGL.cpp
@@ -984,7 +984,7 @@ namespace ftk
                             Size2I frameBufferSize;
                             SDL_GetWindowSize(sdlWindow, &windowSize.w, &windowSize.h);
                             SDL_GetWindowSizeInPixels(sdlWindow, &frameBufferSize.w, &frameBufferSize.h);
-                            window->_sizeUpdate(windowSize, frameBufferSize);
+                            window->_windowUpdate(windowSize, frameBufferSize);
                         }
                     }
                     break;
