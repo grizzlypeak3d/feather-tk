084e6b00448f711484154393670bd1e411c788bb
Darby Johnston <darbyjohnston@yahoo.com>
2026-04-12 15:24:27 -0700

    Add tests

--- tests/CoreTest/LRUCacheTest.cpp
+++ tests/CoreTest/LRUCacheTest.cpp
@@ -28,48 +28,66 @@ namespace ftk
         
         void LRUCacheTest::run()
         {
-            LRUCache<int, bool> c;
-            c.setMax(3);
-            c.setMax(3);
-            FTK_ASSERT(3 == c.getMax());
-            FTK_ASSERT(0 == c.getSize());
-            FTK_ASSERT(0 == c.getCount());
-            _print(Format("Percentage: {0}").arg(c.getPercentage()));
-            
-            c.add(0, true);
-            _print(Format("Percentage: {0}").arg(c.getPercentage()));
-            c.add(1, true);
-            _print(Format("Percentage: {0}").arg(c.getPercentage()));
-            c.add(2, true);
-            _print(Format("Percentage: {0}").arg(c.getPercentage()));
-            c.add(3, true);
-            _print(Format("Percentage: {0}").arg(c.getPercentage()));
-            FTK_ASSERT(3 == c.getSize());
+            {
+                LRUCache<int, bool> c;
+                c.setMax(3);
+                c.setMax(3);
+                FTK_ASSERT(3 == c.getMax());
+                FTK_ASSERT(0 == c.getSize());
+                FTK_ASSERT(0 == c.getCount());
+                _print(Format("Percentage: {0}").arg(c.getPercentage()));
 
-            FTK_ASSERT(c.contains(2));
-            bool v = false;
-            FTK_ASSERT(c.get(2, v));
-            FTK_ASSERT(v);
-            FTK_ASSERT(!c.get(0, v));
-            c.remove(2);
-            FTK_ASSERT(!c.contains(2));
-            c.clear();
-            FTK_ASSERT(0 == c.getSize());
-            
-            c.add(0, true);
-            c.add(1, true);
-            c.add(2, true);
-            c.add(3, true);
-            auto keys = c.getKeys();
-            FTK_ASSERT(std::find(keys.begin(), keys.end(), 1) != keys.end());
-            FTK_ASSERT(std::find(keys.begin(), keys.end(), 2) != keys.end());
-            FTK_ASSERT(std::find(keys.begin(), keys.end(), 3) != keys.end());
-            FTK_ASSERT(c.getValues()[0]);
-            FTK_ASSERT(c.getValues()[1]);
-            FTK_ASSERT(c.getValues()[2]);
-            
-            c.setMax(2);
-            FTK_ASSERT(2 == c.getSize());
+                c.add(0, true);
+                _print(Format("Percentage: {0}").arg(c.getPercentage()));
+                c.add(1, true);
+                _print(Format("Percentage: {0}").arg(c.getPercentage()));
+                c.add(2, true);
+                _print(Format("Percentage: {0}").arg(c.getPercentage()));
+                c.add(3, true);
+                _print(Format("Percentage: {0}").arg(c.getPercentage()));
+                FTK_ASSERT(3 == c.getSize());
+
+                FTK_ASSERT(c.contains(2));
+                bool v = false;
+                FTK_ASSERT(c.get(2, v));
+                FTK_ASSERT(v);
+                FTK_ASSERT(!c.get(0, v));
+                c.remove(2);
+                FTK_ASSERT(!c.contains(2));
+                c.clear();
+                FTK_ASSERT(0 == c.getSize());
+
+                c.add(0, true);
+                c.add(1, true);
+                c.add(2, true);
+                c.add(3, true);
+                auto keys = c.getKeys();
+                FTK_ASSERT(std::find(keys.begin(), keys.end(), 1) != keys.end());
+                FTK_ASSERT(std::find(keys.begin(), keys.end(), 2) != keys.end());
+                FTK_ASSERT(std::find(keys.begin(), keys.end(), 3) != keys.end());
+                FTK_ASSERT(c.getValues()[0]);
+                FTK_ASSERT(c.getValues()[1]);
+                FTK_ASSERT(c.getValues()[2]);
+
+                c.setMax(2);
+                FTK_ASSERT(2 == c.getSize());
+            }
+            {
+                LRUCache<int, bool> c;
+                c.setMax(3);
+                c.add(0, true, 2);
+                FTK_ASSERT(2 == c.getSize());
+                FTK_ASSERT(1 == c.getCount());
+                c.add(1, true, 1);
+                FTK_ASSERT(3 == c.getSize());
+                FTK_ASSERT(2 == c.getCount());
+                c.add(2, true, 1);
+                FTK_ASSERT(2 == c.getSize());
+                FTK_ASSERT(2 == c.getCount());
+                c.add(3, true, 3);
+                FTK_ASSERT(3 == c.getSize());
+                FTK_ASSERT(1 == c.getCount());
+            }
         }
     }
 }
