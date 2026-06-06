1576e6a00960aadaa6bcc8a14f2753c533449f6f
Darby Johnston <darbyjohnston@yahoo.com>
2026-06-05 18:45:10 -0700

    Refactoring

--- tests/ftk-test/ftk-test.cpp
+++ tests/ftk-test/ftk-test.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright Contributors to the feather-tk project.
 
-#include "ftktest.h"
+#include "ftk-test.h"
 
 #include <ftk/UITest/ActionTest.h>
 #include <ftk/UITest/AppTest.h>
