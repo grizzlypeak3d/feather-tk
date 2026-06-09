82d7b78c45f2a2e8240760d00ffa5c2347d4a7f4
Darby Johnston <darbyjohnston@yahoo.com>
2026-06-09 13:02:19 -0700

    Catch exceptions

--- examples/simple/simple.cpp
+++ examples/simple/simple.cpp
@@ -9,21 +9,29 @@ using namespace ftk;
 
 int main(int argc, char** argv)
 {
-    // Create the context and application.
-    auto context = Context::create();
-    auto app = App::create(context, argc, argv, "simple", "Simple example");
-    if (app->getExit() != 0)
-        return app->getExit();
+    try
+    {
+        // Create the context and application.
+        auto context = Context::create();
+        auto app = App::create(context, argc, argv, "simple", "Simple example");
+        if (app->getExit() != 0)
+            return app->getExit();
 
-    // Create a window.
-    auto window = MainWindow::create(context, app, Size2I(1280, 960));
+        // Create a window.
+        auto window = MainWindow::create(context, app, Size2I(1280, 960));
 
-    // Create a label.
-    auto label = Label::create(context, "Hello world");
-    label->setHAlign(HAlign::Center);
-    window->setWidget(label);
+        // Create a label.
+        auto label = Label::create(context, "Hello world");
+        label->setHAlign(HAlign::Center);
+        window->setWidget(label);
 
-    // Run the application.
-    app->run();
+        // Run the application.
+        app->run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "ERROR: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
