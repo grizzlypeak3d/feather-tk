b945b974ff0afd3bcfa71f73a0bc51188efd57a1
Darby Johnston <darbyjohnston@yahoo.com>
2026-08-24 15:57:58 -0700

    writeScreenshot is bound
    Driving a user interface headlessly from Python and reading back what
    it drew is how the Python example is verified.

--- lib/ftk/UIPy/App.cpp
+++ lib/ftk/UIPy/App.cpp
@@ -112,6 +112,7 @@ namespace ftk
                 .def_property_readonly("observeDisplayScale", &App::observeDisplayScale)
                 .def_property("tooltipsEnabled", &App::areTooltipsEnabled, &App::setTooltipsEnabled)
                 .def_property_readonly("observeTooltipsEnabled", &App::observeTooltipsEnabled)
+                .def("writeScreenshot", &App::writeScreenshot, py::arg("path"))
                 .def("exit", &App::exit)
                 .def("run", &App::run)
                 .def("tick", &App::tick);
